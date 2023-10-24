08398d59c5e28dd3dae23fc723326c754eb63b2a
Ambitiousdude <Ugberaeseoa@gmail.com>
2023-10-24 07:20:56 +0000

    Web development codes

--- JAVASCRIPT/portfolio.js
+++ JAVASCRIPT/portfolio.js
@@ -1,13 +1,72 @@
-/*-----About tabs-------*/
-const tabContainer = document.querySelector(".about-tabs");
-aboutSection = document.querySelector(".about-me")
-tabContainer.addEventListener("click", (e) =>{
-    if(e.target.classList.contains("tabs-items") && !e.target.classList.contains("active2")){
-        tabContainer.querySelector(".active2").classList.remove("active2");
-        e.target.classList.add("active2");
-        const target = e.target.getAttribute("data-target");
-        aboutSection.querySelector(".tab-content.active3").classList.remove("active3");
-        aboutSection.querySelector(target).classList.add("active3");
+/*----About section-----*/
 
-    }
+// Get references to the "Education" and "Experience" elements by their IDs
+const educationLink = document.getElementById("education-link");
+const experienceLink = document.getElementById("experience-link");
+
+// Get references to the tab content elements
+const educationTabContent = document.querySelector(".Edu-Exp .tab-content");
+const experienceTabContent = document.querySelector(".Edu-Exp .tab-content:last-child");
+
+// Add click event listeners to the "Education" and "Experience" links
+educationLink.addEventListener("click", function() {
+    // Add or remove the "active-link" class for styling
+    educationLink.classList.add("active-link");
+    experienceLink.classList.remove("active-link");
+
+    // Show the "Education" tab content and hide the "Experience" tab content
+    educationTabContent.style.display = "block";
+    experienceTabContent.style.display = "none";
 });
+
+experienceLink.addEventListener("click", function() {
+    // Add or remove the "active-link" class for styling
+    experienceLink.classList.add("active-link");
+    educationLink.classList.remove("active-link");
+
+    // Show the "Experience" tab content and hide the "Education" tab content
+    experienceTabContent.style.display = "block";
+    educationTabContent.style.display = "none";
+});
+
+// Initialize the active tab content (Education) when the page loads
+educationLink.click();
+
+
+/*----Open and Close menu toggle*/
+const sideMenu = document.getElementById("sidemenu");
+
+
+const openMenu = document.getElementById("openmenu");
+const closeMenu = document.getElementById("closemenu");
+
+openMenu.addEventListener("click", function(){
+    sideMenu.style.right= "0";
+});
+
+
+closeMenu.addEventListener("click", function(){
+    sideMenu.style.right= "-220px";
+});
+
+
+// const scriptURL = 'https://script.google.com/macros/s/AKfycbwy0-_BlrmXKGF6I2Yu12I0cQLG8illcY_FP9IRp2X1hfjFBC0MWISiX-APgyUjvmujNA/exec'
+//     const form = document.forms['submit-to-google-sheet']
+  
+//     form.addEventListener('submit', e => {
+//       e.preventDefault()
+//       fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+//         .then(response => console.log('Success!', response))
+//         .catch(error => console.error('Error!', error.message))
+//     })
+
+/*----Another alternative---*/
+// const sideMenu = document.getElementById("sidemenu");
+
+// function openmenu(){
+//     sideMenu.style.right ="0";
+// }
+
+// function closemenu(){
+//     sideMenu.style.right ="-170px";
+// }
